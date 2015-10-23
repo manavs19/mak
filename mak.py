74b01b7bfe72eb0a8e4c786fdfd02c01f940ed26
manavs19 <manavcool.sethi@gmail.com>
2015-10-23 10:18:10 +0530

    Speech recogntion working ls,open folder

--- mak.py
+++ mak.py
@@ -3,6 +3,7 @@ from os.path import expanduser
 import subprocess
 import time
 import signal
+import speech_recognition as sr
 
 use  = []
 def init():
@@ -50,20 +51,42 @@ def checkOpen():
 			return 1
 	return 0
 
-def cd(folderName):
+def openFolder(folderName):
 	if checkOpen()==0:
 		print "window not open"
 		return #window not open
 	folderNames = getFolderNames()
 	#check case also
-	if folderName not in folderNames:
-		return #folder not found
+	flag=0
+	for f in folderNames:
+		#Get case sensitive name for folder
+		if folderName.lower() == f.lower():
+			folderName = f
+			flag=1
+			break
+
+	if flag==0:#folder not found
+		return
+
 	subprocess.call(["xdotool", "windowactivate", str(use[0])])
 	subprocess.call(["xdotool", "key", "--window", str(use[0]),  "Ctrl+L"])
 	subprocess.call(["xdotool", "type", str(folderName)+"\n"])
 
 	os.chdir(folderName)
 	# print getDirectoryFromAddressBar()
+
+
+def back():
+	if checkOpen()==0:
+		print "window not open"
+		return #window not open
+
+	subprocess.call(["xdotool", "windowactivate", str(use[0])])
+	time.sleep(1)
+	print subprocess.call(["xdotool", "key", "--window", str(use[0]), "alt+Left"])
+
+	# os.chdir(getDirectoryFromAddressBar())
+
 
 
 def getDirectoryFromAddressBar():
@@ -104,20 +127,54 @@ def genericCommand(command):
 # os.kill(p.pid, signal.SIGINT)
 # p.kill()
 
+def exit():
+	if checkOpen():
+		subprocess.call(["wmctrl",  "-i",  "-c", use[0]])
+
 
 if __name__ == "__main__":
 	init()
 	while 1:
-		print "mak>", 
-		s = raw_input().strip().split()
-		print s[0]
-		if s[0]=="ls":
-			ls()
-		elif s[0]=="cd":
-			cd(s[1])
-		elif s[0]=="init":
-			init()
-		# elif s[0] in genericCommands:
-		# 	genericCommand(s[0])
-		elif s[0]=="exit":
-			break
+		try:
+			# obtain audio from the microphone
+			r = sr.Recognizer()
+			with sr.Microphone() as source:
+				print("Say something!")
+				audio = r.listen(source)
+
+			s = ""
+			try:
+				# for testing purposes, we're just using the default API key
+				# to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+				# instead of `r.recognize_google(audio)`
+				s = r.recognize_google(audio)
+				s = s.lower()
+				print("Google Speech Recognition thinks you said " + s)
+			except:
+				#Some error in speech recognition
+				continue
+			# except sr.UnknownValueError:
+			#     print("Google Speech Recognition could not understand audio")
+			# except sr.RequestError as e:
+			#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+			print "mak>", 
+			# s = raw_input().strip().split()
+			# print s[0]
+			s = s.split()
+			if s[0]=="ls":
+				ls()
+			elif s[0]=="open":
+				openFolder(s[1])
+			elif s[0]=="back":
+				back()
+			elif s[0]=="init":
+				init()
+			# elif s[0] in genericCommands:
+			# 	genericCommand(s[0])
+			elif s[0]=="exit":
+				exit()
+				break
+
+		except:#catch any error in while
+			continue
